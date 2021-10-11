b0341ebe04bf5e6c51a884cc80ece5173fcddd80
Riyaz <riyazpasha880@gmail.com>
2021-10-11 17:51:13 +0530

    Add. Dived list into Batches

---
+++ src/java8/lists/Batch.java
@@ -0,0 +1,27 @@
+package java8.lists;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Batch {
+
+    private static <T> Collection<List<T>> nBatches(List<T> objs, final int batchSize) {
+        return IntStream.range(0, objs.size())
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        e -> e % batchSize,
+                        Collectors.mapping(objs::get, toList())
+                ))
+                .values();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> integerList = IntStream.range(1, 11).boxed().collect(toList());
+        Collection<List<Integer>> batches = nBatches(integerList, 3);
+        System.out.println(batches);
+    }
+}
